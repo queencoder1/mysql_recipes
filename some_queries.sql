91abc0aa561c0cb3b1f41203876446ed625b8439
William Flaherty <william.flaherty@gmail.com>
2020-11-19 11:11:33 -0600

    Create some_queries.sql

---
+++ some_queries.sql
@@ -0,0 +1,20 @@
+/* get all the ingredients with names */ 
+SELECT r.name AS 'Recipe', r.instructions, ri.amount AS 'Amount', mu.name AS 'Unit of Measure', i.name AS 'Ingredient' 
+FROM Recipe r 
+JOIN RecipeIngredient ri on r.id = ri.recipe_id 
+JOIN Ingredient i on i.id = ri.ingredient_id 
+LEFT OUTER JOIN Measure mu on mu.id = measure_id;
+
+/*get a list of recipes */ 
+Select * from Recipe
+
+/*
+get a specific ingredient 
+this command is incomplete, the last item on the right hand side of the equals sign needs to be added
+*/ 
+
+SELECT *
+From Recipe r
+JOIN RecipeIngredient ri on ri.recipe_id = r.id  
+JOIN Ingredient i on i.id = ri.ingredient_id 
+WHERE i.name = ???
